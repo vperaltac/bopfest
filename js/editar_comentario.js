cc209de46f45f002771162131fc547a02e442f2c
Diego Garcia <diegogarciaaurelio@gmail.com>
2019-05-21 16:28:04 +0200

    actualizado panel de control

--- js/editar_comentario.js
+++ js/editar_comentario.js
@@ -1,7 +1,9 @@
 const btnEditarC = document.getElementById("editar-comentario");
 const btnBorrarC = document.getElementById("borrar-comentario");
+const btnGuardarC = document.getElementById("guardar-comentario");
 const inTextoE = document.getElementById("inTextoE");
 
 btnEditarC.onclick = function() {
     inTextoE.style.display = "block";
+    btnGuardarC.style.display = "block";
 }

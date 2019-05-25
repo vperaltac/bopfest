fa74887983355c29cdfb3337b06cb6eaae756b20
Diego Garcia <diegogarciaaurelio@gmail.com>
2019-05-25 15:34:00 +0200

    Mejoras al editar eventos

--- js/editar-evento.js
+++ js/editar-evento.js
@@ -6,7 +6,11 @@ const inFecha = document.getElementById("inFecha");
 const inTexto = document.getElementById("inTexto");
 const inImgTitulo = document.getElementById("inImgTitulo");
 const inImgCreditos = document.getElementById("inImgCreditos");
-
+const organizador = document.getElementById("organizador");
+const fecha = document.getElementById("fecha");
+const texto = document.getElementById("texto");
+const tituloImg = document.getElementById("tituloImg");
+const creditosImg = document.getElementById("creditosImg");
 
 btnEditar.onclick = function(){
     btnEditar.style.display = "none";
@@ -17,9 +21,25 @@ btnEditar.onclick = function(){
     inTexto.style.display = "block";
     inImgTitulo.style.display = "block";
     inImgCreditos.style.display = "block";
+    organizador.style.display = "none";
+    fecha.style.display = "none";
+    texto.style.display = "none";
+    tituloImg.style.display = "none";
+    creditosImg.style.display = "none";
 }
 
 btnGuardar.onclick = function() {
     btnGuardar.style.display = "none";
     btnEditar.style.display = "block";
+    inTiutlo.style.display = "none";
+    inOrganizador.style.display = "none";
+    inFecha.style.display = "none";
+    inTexto.style.display = "none";
+    inImgTitulo.style.display = "none";
+    inImgCreditos.style.display = "none";
+    organizador.style.display = "block";
+    fecha.style.display = "block";
+    texto.style.display = "block";
+    tituloImg.style.display = "block";
+    creditosImg.style.display = "block";
 }
